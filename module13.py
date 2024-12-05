f5478e6b08f1e2a9ea718b74a46c545a186f2f25
YUEZHANG-metro <yuezhan@metropolia.fi>
2024-12-05 14:53:33 +0200

    module13 modify

--- module13.py
+++ module13.py
@@ -1,6 +1,9 @@
 ## exercise1
 from flask import Flask, request,jsonify
+from flask_cors import CORS
+import mysql.connector
 app = Flask(__name__)
+CORS(app)
 
 def is_prime(number):
     if number <= 1:
@@ -21,3 +24,31 @@ def prime_or_not(number):
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
+
+app = Flask(__name__)
+CORS(app)
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='spy',
+        password='spy',
+        database='spy'
+    )
+    return connection
+@app.route('/get_ICAO/<icao>',methods= ['GET'])
+def get_icao_from_database(icao):
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f" SELECT ident AS ICAO, name AS Name, municipality AS Location FROM airport WHERE ident = %s",
+            (icao,))
+    airport_data = cursor.fetchone()
+    cursor.close()
+    connection.close()
+
+    if airport_data:
+        return jsonify(airport_data)
+    else:
+        return jsonify({"error": "Country not found"}), 404
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=3000)
